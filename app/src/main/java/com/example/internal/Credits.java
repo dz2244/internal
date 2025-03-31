7911e5c89b2b75e638acfc9fa16db2a0b3474433
dz2244 <dz2244@bs.amalnet.k12.il>
2025-03-31 15:56:53 +0300

    a

--- app/src/main/java/com/example/internal/Credits.java
+++ app/src/main/java/com/example/internal/Credits.java
@@ -1,10 +1,10 @@
 package com.example.internal;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-public class MainActivity extends AppCompatActivity {
+public class Credits extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_credits);
     }
 }
