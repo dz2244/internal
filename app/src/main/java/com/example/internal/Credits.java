fbf8d4e0d3d27d68c2f5a507dee511316561d695
dz2244 <dz2244@bs.amalnet.k12.il>
2025-04-02 16:26:29 +0300

    a

--- app/src/main/java/com/example/internal/Credits.java
+++ app/src/main/java/com/example/internal/Credits.java
@@ -1,10 +1,30 @@
 package com.example.internal;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 public class Credits extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@Nullable MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.main) {
+            finish();
+        }
+
+        return true;
     }
 }
